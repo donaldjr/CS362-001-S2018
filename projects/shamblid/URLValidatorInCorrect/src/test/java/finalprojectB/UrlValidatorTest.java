56fce25caeedc5eab94a6500a3a274f3563b75b8
DJ <hamblindj@gmail.com>
2018-06-11 22:41:23 -0700

    added programmatic testing

--- projects/shamblid/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/shamblid/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -21,22 +21,6 @@ public class UrlValidatorTest extends TestCase {
    }
 
    
-      ResultPair[] testUrlScheme = { new ResultPair("http://", true), new ResultPair("ftp://", true),
-      new ResultPair("3ht://", false), new ResultPair("http:", false), new ResultPair("://", false) };
-  
-      ResultPair[] testUrlAuthority = { new ResultPair("www.google.com", true), new ResultPair("go.com", true),
-      new ResultPair("256.256.256.256", true), new ResultPair("1.2.3.4.5", false), new ResultPair("aaa", false) };
- 
-      ResultPair[] testUrlPort = { new ResultPair(":80", true), new ResultPair(":65535", true), new ResultPair("", true),
-      new ResultPair(":-1", false), new ResultPair(":65a", false) };
-  
-      ResultPair[] testPath = { new ResultPair("/test1", true), new ResultPair("/t123", true), new ResultPair("", true),
-      new ResultPair("/..", false), new ResultPair("/..//file", false) };
- 
-      ResultPair[] testUrlQuery = { new ResultPair("?action=view", true), new ResultPair("?action=edit&mode=up", true),
-      new ResultPair("", true) };
-
-   
 
    @Test
    public void testManualTest() {
@@ -125,6 +109,66 @@ public class UrlValidatorTest extends TestCase {
    @Test
    public void testIsValid() {
        // You can use this function for programming based testing
+        // You can use this function for programming based testing
+        ResultPair[] testUrlScheme = { new ResultPair("http://", true), new ResultPair("ftp://", true),
+                new ResultPair("3ht://", false), new ResultPair("http:", false), new ResultPair("://", false) };
+
+        ResultPair[] testUrlAuthority = { new ResultPair("www.google.com", true), new ResultPair("go.com", true),
+                new ResultPair("256.256.256.256", true), new ResultPair("1.2.3.4.5", false),
+                new ResultPair("aaa", false) };
+
+        ResultPair[] testUrlPort = { new ResultPair(":80", true), new ResultPair(":65535", true),
+                new ResultPair("", true), new ResultPair(":-1", false), new ResultPair(":65a", false) };
+
+        ResultPair[] testPath = { new ResultPair("/test1", true), new ResultPair("/t123", true),
+                new ResultPair("", true), new ResultPair("/..", false), new ResultPair("/..//file", false) };
+
+        ResultPair[] testUrlQuery = { new ResultPair("?action=view", true),
+                new ResultPair("?action=edit&mode=up", true), new ResultPair("", true) };
+
+        UrlValidator urlValidator1 = new UrlValidator(null); // default schemes to use with
+                                                                                       // these pairs
+        // UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            StringBuilder buff = new StringBuilder();
+            boolean expected = true;
+
+            int schemeIdx = random.nextInt(5); // get one of the 5 pairs
+            buff.append(testUrlScheme[schemeIdx].item); // append the index of the pair at the buffer
+            expected &= testUrlScheme[schemeIdx].valid;
+
+            int authIdx = random.nextInt(5);
+            buff.append(testUrlAuthority[authIdx].item);
+            expected &= testUrlAuthority[authIdx].valid;
+
+            int portIdx = random.nextInt(5);
+            buff.append(testUrlPort[portIdx].item);
+            expected &= testUrlPort[portIdx].valid;
+
+            int pathIdx = random.nextInt(5);
+            buff.append(testPath[pathIdx].item);
+            expected &= testPath[pathIdx].valid;
+
+            int queryIdx = random.nextInt(3);
+            buff.append(testUrlQuery[queryIdx].item);
+            expected &= testUrlQuery[queryIdx].valid;
+
+            String finalUrl = buff.toString();
+            boolean result1 = urlValidator1.isValid(finalUrl);
+            // boolean result2 = urlValidator2.isValid(finalUrl);
+
+            System.out.println("Final url:" + finalUrl + " Result1: " + result1);
+            if (result1 == true) {
+                System.out.println("Default scheme validator good!");
+            }
+
+            // if (result2 == true){
+            // System.out.println("All scheme validator good!");
+            // }
+        }
+
    }
 
 
